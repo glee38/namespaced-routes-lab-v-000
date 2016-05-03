a21eacf87210c19d39768bc3e9fb58e405b5576b
glee38 <glee3388@gmail.com>
2016-05-03 18:58:52 -0400

    tried to implement sort - works in localhost, fails learn tests, so removed from views

--- app/helpers/artists_helper.rb
+++ app/helpers/artists_helper.rb
@@ -2,13 +2,4 @@ module ArtistsHelper
   def display_artist(song)
     song.artist.nil? ? link_to("Add Artist", edit_song_path(song)) : link_to(song.artist_name, artist_path(song.artist))
   end
-
-  def sort_artists(preference)
-    if preference.artist_sort_order.downcase == "asc"
-      Artist.order(name: :asc)
-    elsif preference.artist_sort_order.downcase == "desc"
-      Artist.order(name: :desc)
-    end
-  end
-
 end
--- app/models/preference.rb
+++ app/models/preference.rb
@@ -1,10 +1,3 @@
 class Preference < ActiveRecord::Base
 
-  def self.song_sort_order
-    @order
-  end
-
-  def self.song_sort_order=(order)
-    @order = order
-  end
 end
